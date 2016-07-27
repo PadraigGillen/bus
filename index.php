90440ce63104e389c8fdc5ded411f7556db1a623
PadraigGillen <PadraigGillen@users.noreply.github.com>
2016-07-27 11:13:53 -0700

    support for route numbers; formatting

--- index.php
+++ index.php
@@ -1,25 +1,22 @@
 <html>
 <head>
-<link rel="SHORTCUT ICON" HREF="https://upload.wikimedia.org/wikipedia/commons/8/80/TransLink-op-head-bus-right.png">
-<meta http-equiv="refresh" content="15"/>
-<style>
-body{
-    font-size: 45pt;
-}
-</style>
-
-<?php
+    <link rel="SHORTCUT ICON" HREF="https://upload.wikimedia.org/wikipedia/commons/8/80/TransLink-op-head-bus-right.png">
+    <meta http-equiv="refresh" content="15"/>
+    <style>
+        body{
+            font-size: 45pt;
+        }
+    </style>
+    <?php
     if (!isset($_GET["id"])) {
         die("Please enter an id.");
-    } else {
-        $stop = $_GET["id"];
     }
-
+    $stop = $_GET["id"];
     echo "<title>$stop</title>\n";
 ?>
 </head>
 <body>
-<center>
+    <center>
 <?php
 
     #$xmlFile = "sample.xml";
@@ -29,14 +26,19 @@ body{
     if (isset($xml->Platform->Route)) {
         $trip_arr = (array) $xml->Platform->Route->Destination->Trip;
         $eta = $trip_arr["@attributes"]["ETA"];
+        
+        $route_arr = (array) $xml->Platform->Route;
+        $route = $route_arr["@attributes"]["RouteNo"];
+
     } else {
         $eta = "&gt; 30";
+        $route = "unknown";
     }
 
-    echo "<h4>$stop</h4>\n";
-    echo "<h1><b>$eta min</b></h1>\n";
+    echo "        <h4>$stop - Route $route</h4>\n";
+    echo "        <h1><b>$eta min</b></h1>\n";
 
 ?>
-</center>
+    </center>
 </body>
 </html>
