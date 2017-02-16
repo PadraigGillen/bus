28f18ba71dfc30e12d59895120ae3a658822e457
Padraig Gillen <gillenp@oregonstate.edu>
2017-02-16 15:27:32 -0800

    add the status quo, still needs work

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
-    # verify URL has valid parameter
+    // this entire file needs to be refactored, currently it's been developed idea by idea
+    // verify URL has valid parameter
     $numeric_id = false;
     if ( isset($_GET["id"]) && ctype_digit($_GET["id"]) ) {
         $numeric_id = true;
         $stop = $_GET["id"];
     }
+    $iconTracking = isset($_GET["trrt"]);
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-<link rel="SHORTCUT ICON" HREF="bus.png">
+<link id="favicon" rel="shortcut icon" type="image/png" href="icons/gen.png">
+<meta charset="utf-8">
 <style>
 main {
     font-size: 3em;
@@ -42,7 +46,8 @@ if ($numeric_id) {
 
         # track when the next bus is coming
         $minEta = 30;
-
+        $trackedEta = 31;
+        $routeFound = False;
         # check if any busses are coming
         if (isset($xml->Platform->Route)) {
             # iterate through each bus and display info
@@ -52,8 +57,15 @@ if ($numeric_id) {
 
                 $tripArr = (array) $routeObj->Destination->Trip;
                 $eta = $tripArr["@attributes"]["ETA"];
+                if ($routeNum == $_GET["trrt"]) {
+                    $trackedEta = $eta;
+                }
                 if ($eta < $minEta) $minEta = $eta;
                 echo "<h2>Rt $routeNum - $eta min</h2>\n";
+            } 
+            # have the icon track the bus arrival time if there's only 1
+            if ( (count($xml->Platform->Route) == 1) && !($iconTracking) ) {
+                $iconTracking = True;
             }
             echo "<a href='/~gillenp/bus' class='footnote'>Choose a different stop</a>\n";
         } else {
@@ -69,6 +81,10 @@ if ($numeric_id) {
         # refresh page to quickly get new results
         $refreshRate = ($minEta < 4) ? 15 : 30;
         echo "<meta http-equiv='refresh' content='$refreshRate'/>\n";
+        if ($iconTracking) {
+            //if ($trackedEta) $trackedEta = $minEta;
+            echo "<script>document.getElementById('favicon').href = 'icons/".$trackedEta."min.png';</script>";
+        }
     } else {
         echo "<h3>-Invalid stop entered-</h3>\n";
         displayStopInputForm();
